d280671bb94011e360d01467af8d034d0e8cb3d6
Anurag Agarwal <anuragagarwal561994@users.noreply.github.com>
2023-01-29 02:41:55 +0530

    Puts common logic together as generics

---
+++ instrumentation/apache-httpclient/commons/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/commons/OtelHttpRequest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.apachehttpclient.commons;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public interface OtelHttpRequest {
+  BytesTransferMetrics getBytesTransferMetrics();
+
+  String getPeerName();
+
+  Integer getPeerPort();
+
+  InetSocketAddress getPeerSocketAddress();
+
+  String getMethod();
+
+  String getUrl();
+
+  String getFlavor();
+
+  List<String> getHeader(String name);
+
+  String getFirstHeader(String name);
+
+  void setHeader(String key, String value);
+}
